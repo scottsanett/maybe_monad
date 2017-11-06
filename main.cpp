3150015c9cf4335ba28c6c1f16a7e8cebc0392d4
Li Xianpeng <131100007@mail.smail.nju.edu.cn>
2017-11-06 18:22:32 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "maybe.hpp"
+#include <iostream>
+
+auto add(Maybe<int> mx, Maybe<int> my) -> Maybe<int> {
+    return mx >>= [&my](auto x){
+        return my >>= [&x](auto y) {
+            return (x + y);
+        };
+    };
+}
+
+auto divide(Maybe<int> mx, Maybe<int> my) -> Maybe<int> {
+    return mx >>= [&my](auto x){
+        return my >>= [&x](auto y) {
+            return (y == 0) ? Maybe<int>{} : (x / y);
+        };
+    };
+}
+
+int main() {
+    std::cout << add(Maybe<int>{100}, Maybe<int>{1000}).get() << std::endl;
+    std::cout << divide(Maybe<int>{1000}, Maybe<int>{0}).is_valid() << std::endl;
+}
---
+++ maybe.hpp
@@ -0,0 +1,44 @@
+#include <type_traits>
+#include <variant>
+#include <iostream>
+
+template <typename T> struct Maybe;
+
+template <typename T>
+struct Maybe {
+    template <typename D, typename Callable>
+    friend Maybe<D> operator >>= (Maybe<D>, Callable);
+
+private:
+    T m_value;
+    bool m_valid;
+
+public:
+    using type = T;
+    Maybe(T t): m_value(t), m_valid(true) {}
+    Maybe(): m_valid(false) {}
+    Maybe(Maybe const & rhs) = default;
+    Maybe(Maybe && rhs) noexcept = default;
+    ~Maybe() = default;
+    Maybe operator= (Maybe const & rhs) {
+        *this = rhs;
+        return *this;
+    }
+    Maybe operator= (Maybe && rhs) {
+        *this = std::move(rhs);
+        return *this;
+    }
+    T get() { if (m_valid) { return m_value; } }
+    bool is_valid() { return m_valid; }
+};
+
+template <typename T, typename Callable>
+Maybe<T> operator >>= (Maybe<T> m, Callable f) {
+    if (!m.is_valid()) {
+        return Maybe<T>{};
+    }
+    else {
+        return Maybe<T>(f(m.get()));
+    }
+}
+
